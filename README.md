e73313191d3ff54605e535b0a9dd4912c7a2a847
cdw33 <cdw33@zips.uakron.edu>
2015-04-15 14:09:05 -0400

    config files moved to their respective dirs

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ Installation script for BSPWM
 
 Distributions
 Currently uses apt-get, so only working in Debian based distros
+
+
+More Information
+github.com/windelicato/dotfiles/wiki/bspwm-for-dummies
--- install.py
+++ install.py
@@ -7,6 +7,7 @@
 ##################################################
 
 import os
+#import shutil
 import sys #used for sys.exit() in debugging
 
 bspwm_git_dir = "https://github.com/baskerville/bspwm"
@@ -15,11 +16,20 @@ xtitle_git_dir = "https://github.com/baskerville/xtitle"
 sutils_git_dir = "https://github.com/baskerville/sutils"
 lemonbar_git_dir = "https://github.com/LemonBoy/bar"
 
-panel_scripts_dir = "~/.config/panel"
-bspwm_scripts_dir = "~/.config/bspwm"
-sxkhd_scripts_dir = "~/.config/sxkhd"
+config_dir = "~/.config"
+panel_scripts_dir = "~/.config/panel/"
+bspwm_scripts_dir = "~/.config/bspwm/"
+sxhkd_scripts_dir = "~/.config/sxhkd/"
 
-tmp_dir = "/tmp/bspwm_installer"
+bspwmrc_dir = "bspwm/examples/bspwmrc"
+sxhkdrc_dir = "bspwm/examples/sxhkdrc"
+panel_config_dir = "bspwm/examples/panel/"
+
+tmp_dir = "/tmp/bspwm_installer/"
+
+arch_pm = "sudo pacman -S"
+debian_pm = "sudo apt-get install"
+
 
 def mkdir(d):
     if not os.path.exists(d):
@@ -39,14 +49,15 @@ os.chdir(tmp_dir)
 #get source files via git
 print "Downloading Source..."
 os.system("git clone %s" %bspwm_git_dir);
-#this is a bug with the current bspwm build that causes 
-#an xserver connection error on Make. This is fixed by 
-#checking out a previous version
+#there is a bug with the current bspwm build that causes 
+#an Xserver connection error when using Make. The current
+#workaround is by checking out and older version
 os.system("cd bspwm && git checkout b0e8dd3 && cd ..")
 os.system("git clone %s" %sxhkd_git_dir);
-os.system("git clone %s" %xtitle_git_dir);
-os.system("git clone %s" %sutils_git_dir);
-os.system("git clone %s" %lemonbar_git_dir);
+#git Panel Source Files
+#os.system("git clone %s" %xtitle_git_dir);
+#os.system("git clone %s" %sutils_git_dir);
+#os.system("git clone %s" %lemonbar_git_dir);
 print "Download Complete\n"
 
 #Download any necessary dependencies
@@ -61,26 +72,36 @@ print "Download Complete\n"
 print "Building Source..."
 os.system("make -C bspwm/ && sudo make -C bspwm/ install")
 os.system("make -C sxhkd/ && sudo make -C sxhkd/ install")
-os.system("make -C xtitle/ && sudo make -C sutils/ install")
-os.system("make -C bspwm/ && sudo make -C bspwm/ install")
-os.system("make -C bar/ && sudo make -C bar/ install")
+#Build Panel Source
+#os.system("make -C xtitle/ && sudo make -C sutils/ install")
+#os.system("make -C bspwm/ && sudo make -C bspwm/ install")
+#os.system("make -C bar/ && sudo make -C bar/ install")
 print "Build Complete\n"
 
-#move files to thier final location &
-#edit necessary files accoringly
+#Deploy Files to System
 print "Deploying System..."
 #~/.config/panel/panel,panel_bar, panel_colors
 #~/.config/bspwm/bspwm
 #~/.config/sxhkd/sxhkdrc
-
 #edit $PATH to include panel location
 #add PANEL_FIFO=/tmp/panel_fifo to /etc/profile(?)
+mkdir(config_dir)
+mkdir(bspwm_scripts_dir)
+mkdir(sxhkd_scripts_dir)
+try:
+    #shutil.copy(bspwmrc_dir, bspwm_scripts_dir)
+    os.system("cp %s %s" %(bspwmrc_dir, bspwm_scripts_dir))
+    os.system("cp %s %s" %(sxhkdrc_dir, sxhkd_scripts_dir))
+    print "all files moved successfully"
+except Exception, msg:
+    print "move failed with error: %s" %msg
 print "System Deploymen Complete\n"
 
 
-#cleanup my mess
+#Cleanup
 #os.rmdir("tmp") //file not empty (shutil.rmtree?)
 print "Cleaning Up..."
 os.system("rm -rf %s" %tmp_dir)
 print "Cleanup Complete.\n"
+
 print "BSPWM is now installed!"
